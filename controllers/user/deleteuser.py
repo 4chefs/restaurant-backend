d4882e80cfb91b76e4eba86a7bc56bc4f08a0ad6
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-01 07:44:49 -0300

    FEAT: Criando o deleteuser.py

--- controllers/user/deleteuser.py
+++ controllers/user/deleteuser.py
@@ -1,4 +1,24 @@
 from models.usermodel import Usuario
+from flask import request
+from errors.delete_error import DeleteError
+from utils.database_instance import db
 
-def delete_user() -> Usuario:
-    pass
+def delete_user(user_id) -> Usuario:
+    """
+    Apaga um usuário com base no id recebido.
+
+    Returns:
+        Objeto da classe Usuario
+
+    Raises:
+        DeleteError: Se o usuário não existir ou os dados forem inválidos.
+    """
+    usuario: Usuario = Usuario.query.get(user_id)
+
+    if not usuario:
+        raise DeleteError("<pre>Usuário não encontrado.</pre>")
+    
+    db.session.remove(usuario)
+    db.session.commit()
+
+    return usuario
---
+++ errors/delete_error.py
@@ -0,0 +1,7 @@
+class DeleteError(Exception):
+    def __init__(self, mensagem):
+        super().__init__(mensagem)
+        self.mensagem = mensagem
+    
+    def __str__(self):
+        return f"Deletion Error: {self.mensagem}"
